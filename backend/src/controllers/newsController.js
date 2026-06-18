a463b2d1ef39980358af1a0c42e7a6ec0c3c1275
Angad Thakur <angadthakur2004@gmail.com>
2026-06-18 10:59:14 +0530

    NewsController file to extract the news from the newsAPI

--- backend/src/controllers/newsController.js
+++ backend/src/controllers/newsController.js
@@ -3,24 +3,49 @@ const axios = require ('axios');
 const getTopHeadlines =  async (req,res) => {
     try{
         //optional query parameters 
-        const category =  req.query.category || 'general';
-        const country = req.query.country || 'us';
+        let category =  req.query.category || 'general';
 
-        //fetch from real external NewsAPI 
-        const response =  await axios.get(`https://newsapi.org/v2/top-headlines`,{
-            params:{
-                country: country,
-                category: category,
-                apiKey: process.env.NEWS_API_KEY
+        if(category.toLowerCase() === 'top news'){
+            category = 'general';
+        }
+
+        const globalCountryCodes = ['in' , 'us', 'gb', 'au', 'ca'];
+        const futures = [];
+        
+
+        for(const countryCode of globalCountryCodes){
+            const url = `https://newsapi.org/v2/top-headlines`;
+            futures.push(axios.get(url,{
+                params : {
+                    country: countryCode,
+                    category: category,
+                    apiKey: process.env.NEWS_API_KEY
+                }
+            }));
+        }
+
+        const results = await Promise.all(futures);
+
+        //extracting articles from all responses and combine into one big array 
+        let allArticles = [];
+        results.forEach(response => {
+            if(response.data && response.data.articles){
+                allArticles = allArticles.concat(response.data.articles);
             }
         });
+        
+        //articles with no url
+        allArticles = allArticles.filter(article => article.url);
 
-        res.status(200).json(response.data);
-    }catch(error){
-        console.error(error.message);
-        //if newsapi fails
-        res.status(502).json({ message: 'Failed to fetch news from upstream provider' });
+        //array shuffle
+        allArticles.sort(() => Math.random() - 0.5);
+
+        res.status(200).json({articles : allArticles});
+    }catch (error){
+        console.error("Backend Error fetching news:",error.message);
+        res.status(502).json({message : 'Failed to fetch news from upstream provider' })
     }
 };
+
 
 module.exports = { getTopHeadlines };
